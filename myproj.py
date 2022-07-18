c5fe421ccfeff9b7cc61785e9e5aa9c07c2cfc79
pradeep83473 <pradeepsingh83473@gmail.com>
2022-07-19 03:08:00 +0530

    my commits

---
+++ myproj.py
@@ -0,0 +1,21 @@
+#!C:/Python39/python
+import cgi
+import mysql.connector
+
+f = cgi.FieldStorage()
+fname = f.getvalue("fname")
+phone = f.getvalue("phone")
+email = f.getvalue("email")
+msg = f.getvalue("msg")
+
+con = mysql.connector.connect(user="root",
+                              password='',
+                              host="localhost",
+                              database="contact")
+cur = con.cursor()
+
+cur.execute("insert into ppl values(%s,%s,%s,%s)", (fname, phone, email, msg))
+con.commit()
+
+cur.close()
+con.close()
